ec25e349bde1665ca07a3d0559526bf6b29affe8
Ethan Black <ethanrossblack@gmail.com>
2023-07-10 09:43:43 -0600

    test: edit election test

--- spec/features/elections/new_spec.rb
+++ spec/features/elections/new_spec.rb
@@ -1,16 +1,5 @@
 require "rails_helper"
 
-# As a visitor
-# When I visit the Parent Index page
-# Then I see a link to create a new Parent record, "New Parent"
-# When I click this link
-# Then I am taken to '/parents/new' where I see a form for a new parent record
-# When I fill out the form with a new parent's attributes:
-# And I click the button "Create Parent" to submit the form
-# Then a POST request is sent to the '/parents' route,
-# a new parent record is created,
-# and I am redirected to the Parent Index page where I see the new Parent displayed.
-
 describe "New Election", type: :feature do
   
   before :each do
