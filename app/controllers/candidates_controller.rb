bd90420ee89162fdba52d763e874458549bafe7d
Ethan Black <ethanrossblack@gmail.com>
2023-07-11 00:33:33 -0600

    feat: update method for candidates_controller and candidate_params private method

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -9,5 +9,18 @@ class CandidatesController < ApplicationController
 
   def edit
     @candidate = Candidate.find(params[:id])
-  end  
+  end
+
+  def update
+    candidate = Candidate.find(params[:id])
+    candidate.update(candidate_params)
+    candidate.save
+    redirect_to "/candidates/#{candidate.id}"
+  end
+  
+  private
+    def candidate_params
+      params[:candidate].permit(:name, :votes, :incumbent)
+    end
 end
+
