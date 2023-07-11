ad3c0cc0f747dc3cc028a7bc46909839b230db3c
Ethan Black <ethanrossblack@gmail.com>
2023-07-10 23:58:30 -0600

    feat: add edit candidates_controller method

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,4 +6,8 @@ class CandidatesController < ApplicationController
   def show
     @candidate = Candidate.find(params[:id])
   end
+
+  def edit
+    @candidate = Candidate.find(params[:id])
+  end  
 end
